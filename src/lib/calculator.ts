05ffe9a72ae192801473e7edb4b33f11875b43ba
callsamu <samuelbree@gmail.com>
2023-04-11 15:46:38 -0300

    fix: make calculator.ts return a result object

--- src/lib/calculator.ts
+++ src/lib/calculator.ts
@@ -6,13 +6,17 @@ export interface Result {
   chance: number;
 }
 
-export function computeChance(name1: string, name2: string): number {
+export function computeChance(name1: string, name2: string): Result {
   const limit = 10_000;
 
   const m = hash.hashString(name1, limit);
   const n = hash.hashString(name2, limit);
   
-  return hash.hashNumber(m + n, limit) / 100;
+  return {
+    first: name1,
+    second: name2,
+    chance: hash.hashNumber(m + n, limit) / 100
+  };
 }
 
 
