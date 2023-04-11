48be50e64065802919335f610fbf78f9c63382d5
callsamu <samuelbree@gmail.com>
2023-04-11 20:44:51 -0300

    feat: add open-graph metadata

--- src/components/SiteHead.tsx
+++ src/components/SiteHead.tsx
@@ -4,11 +4,20 @@ interface Props {
     title: string;
 }
 
+const description = "Descubre la verdadera compatibilidad con tu pareja con nuestro Amorómetro basado en numerología. ¡Obtén resultados precisos y confiables en segundos!";
+
 export default function SiteHead({ title }: Props) {
+
     return (
         <Head>
             <title>{title}</title>
-            <meta name="description" content="Descubre la verdadera compatibilidad con tu pareja con nuestro Amorómetro basado en numerología. ¡Obtén resultados precisos y confiables en segundos!" />
+
+            <meta name="og:title" content="Amorometro" />
+            <meta name="og:type" content={description} />
+            <meta name="og:image" content="/open-graph-image.png" />
+            <meta name="og:url" content="https://amorometro.pages.dev/" />
+
+            <meta name="description" content={description} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <link rel="icon" href="/favicon.ico" />
         </Head>
