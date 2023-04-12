69e371f1bd08fc8f6a0b1b5c38b212080f48867a
callsamu <samuelbree@gmail.com>
2023-04-12 14:04:38 -0300

    feat: use react web share

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,6 +1,7 @@
 import { Result } from "@/lib/calculator";
 import { stripIdents } from "@/lib/text";
 import { useEffect, useState } from "react";
+import { RWebShare } from "react-web-share";
 
 type Handler = () => void;
 
@@ -10,28 +11,24 @@ type Props = {
 }
 
 export default function ShareButton({ className, results }: Props) {
-  let [canShare, setCanShare] = useState(false);
-  let [handleClick, setHandleClick] = useState<Handler>(() => {});
+  const [url, setUrl] = useState("https://amorometro.pages.dev/");
 
-  useEffect(() => {
-     const shareData = {
-        url: window.location.href,
-        text: stripIdents(`
-          Según amorometro, la compatibilidad entre ${results.first} 
-          e ${results.second} es del ${results.chance}%. 
-          ¡Ven y descubre lo compatible que eres con tu crush!
-        `)
-      };
+  const description = stripIdents(`        
+    Según amorometro, la compatibilidad entre ${results.first} 
+    e ${results.second} es del ${results.chance}%. 
+    ¡Ven y descubre lo compatible que eres con tu crush!
+  `);
 
-    if (navigator.share && navigator.canShare()) {
-      setHandleClick(() => navigator.share(shareData));
-      setCanShare(true);
-    }
-  });
+  useEffect(() => setUrl(window.location.href), []);
 
-  if (canShare) {
-    return <button className={className} onClick={handleClick}> Compartir </button>;
-  }
+  const data = {
+    url: url,
+    text: description,
+  };
 
-  return <></>;
+  return (
+    <RWebShare data={data}>
+      <button className={className}> Compartir </button>
+    </RWebShare>
+  );
 }
