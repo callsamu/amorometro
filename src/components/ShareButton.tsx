9a47f21076481c361870d44886b4f3eb791e1d6d
callsamu <samuelbree@gmail.com>
2023-04-11 23:42:47 -0300

    fix: handle case where window is undefined

--- src/components/ShareButton.tsx
+++ src/components/ShareButton.tsx
@@ -1,5 +1,6 @@
 import { Result } from "@/lib/calculator";
 import { stripIdents } from "@/lib/text";
+import { useEffect } from "react";
 
 type Props = {
   className: string;
@@ -7,22 +8,28 @@ type Props = {
 }
 
 export default function ShareButton({ className, results }: Props) {
-  if (!navigator.canShare()) return <></>;
+  let canShare = false;
+  let handleClick = () => {};
 
-  const handleClick = () => {
-    navigator.share({
-      url: "https://amorometro.pages.dev",
-      text: stripIdents(`
-        Según amorometro, la compatibilidad entre ${results.first} 
-        e ${results.second} es del ${results.chance}%. 
-        ¡Ven y descubre lo compatible que eres con tu crush!
-      `)
-    });
+  const shareData = {
+    url: "https://amorometro.pages.dev",
+    text: stripIdents(`
+      Según amorometro, la compatibilidad entre ${results.first} 
+      e ${results.second} es del ${results.chance}%. 
+      ¡Ven y descubre lo compatible que eres con tu crush!
+    `)
+  };;
+
+  useEffect(() => {
+    if (navigator.share && navigator.canShare()) {
+      handleClick = () => navigator.share(shareData);
+      canShare = true;
+    }
+  }, []);
+
+  if (canShare) {
+    return <button className={className} onClick={handleClick}> Compartir </button>;
   }
 
-  return (
-    <button className={className} onClick={handleClick}>
-      Compartir
-    </button>
-  );
+  return <></>;
 }
