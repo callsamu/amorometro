d11d0b5f7fbc53c3047e520793256c7955fe3e4a
callsamu <samuelbree@gmail.com>
2023-04-15 22:29:00 -0300

    feat: add SiteBody toggle animation on results page

--- src/pages/amor.tsx
+++ src/pages/amor.tsx
@@ -7,6 +7,7 @@ import Counter from "@/components/Counter";
 import getResultTexts from "@/lib/text";
 import styles from "@/styles/Amor.module.css";
 import ShareButton from "@/components/ShareButton";
+import { useEffect, useRef, useState } from "react";
 
 enum Order {
   First,
@@ -30,6 +31,13 @@ function getName(router: NextRouter, order: Order): string {
 export default function Amor() {
   const router = useRouter()
 
+  const [height, setHeight] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const heart  = useRef<HTMLDivElement>(null);
+  const UI     = useRef<HTMLDivElement>(null);
+
+
   const result = computeChance(
     getName(router, Order.First), 
     getName(router, Order.Second)
@@ -37,21 +45,46 @@ export default function Amor() {
 
   const handleClick = () => router.push("/")
 
+  useEffect(() => {
+    if (!heart.current) return;
+
+    // Hardcoded
+    const padding = 15 * 2;
+    setHeight(heart.current.offsetHeight + padding);
+
+    setTimeout(() => { 
+      if (!UI.current || !heart.current) return;
+      setIsVisible(true);
+      setHeight(heart.current.offsetHeight + UI.current.offsetHeight + padding);
+
+      setTimeout(() => {
+        setHeight(0);
+      }, 3000)
+    }, 5000);
+  }, []);
+
   return (
     <>
       <SiteHead title={`${result.first} + ${result.second} | Amorometro`}/>
-      <SiteBody>
-        <h1>{result.first} + {result.second} </h1>
-        <div className={styles.heartContainer}>
-          <img src="/heart.webp" width="200" height="200" alt="heart" />
-          <h2 className={styles.counter}>
-            <Counter ceiling={result.chance} duration={5000} />%
-          </h2>
+      <SiteBody style={{ 
+        height: height === 0 ? "auto" : height, 
+        transition: !isVisible ? "none" : "height 2s ease-in-out"
+      }}>
+        <div ref={heart}>
+          <h1>{result.first} + {result.second} </h1>
+          <div className={styles.heartContainer}>
+            <img src="/heart.webp" width="200" height="200" alt="heart" />
+            <h2 className={styles.counter}>
+              <Counter ceiling={result.chance} duration={5000} />%
+            </h2>
+          </div>
         </div>
-        <Paragraph className={styles.text}> {getResultTexts(result)} </Paragraph>
-        <button onClick={handleClick} className={styles.button}>Regressar</button>
-        <ShareButton results={result} className={styles.button} />
-      </SiteBody>
+        <div ref={UI} style={{ display: !isVisible ? "none" : "block" }}>
+          <Paragraph className={styles.text}> {getResultTexts(result)} </Paragraph>
+          <button onClick={handleClick} className={styles.button}>Regressar</button>
+          <ShareButton results={result} className={styles.button} />
+        </div>
+     </SiteBody>
     </>
   );
 }
